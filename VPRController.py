4ea6bd55c7c3987b8f07589dfbe2986963e80af8
SRGTxTwinkie <zanedreisbig@yahoo.com>
2022-05-25 14:34:26 -0400

    first commit

---
+++ VPRController.py
@@ -0,0 +1,114 @@
+from enum import Enum
+from re import T
+from pywinauto import application, ElementAmbiguousError
+from time import sleep
+
+class VPRTypes(Enum):
+    VPR_FORM = 0
+    VPR_X12 = 1
+
+
+class VPRController:
+    def __init__(self) -> None:
+        self.applicationReference = application.Application()
+        try:
+            self.reference = self.applicationReference.connect(title_re="Viewer Printer")
+            self.vpReference = self.reference.ViewerPrinter
+        except:
+            raise Exception("Could not connect to Viewer Printer...\nMaybe not open?\nError Dialouge open?")
+
+        self.currentTabState = self._determinePage()
+
+    def _determinePage(self):
+        edit3Text = self.vpReference.Edit3.window_text()
+        if len(edit3Text) == 12 or len(edit3Text) == 0:
+            print("VPR Form Selected")
+            print("If wrong - Call invertTabState().")
+            return VPRTypes.VPR_FORM
+        else:
+            print("VPR X12 Selected")
+            print("If wrong - Call invertTabState().")
+            return VPRTypes.VPR_X12
+
+    def invertTabState(self):
+        '''
+        Cycles tab state
+        '''
+        if self.tabState == VPRTypes.VPR_FORM:
+            self.tabState = VPRTypes.VPR_X12
+        else:
+            self.tabState = VPRTypes.VPR_FORM
+
+    def updateClaimNum(self, claimNum:str, search:bool=False, checkWrongClaim:bool=True, timeout:float|int=3, log:bool=False):
+        '''
+        Updates field labeled "DCN" with arg -> claimNum
+        args:
+            claimNum:str -> updated claim number
+            search:bool=False -> click on search button
+            log:bool=False -> print log information
+        '''        
+        for _ in range(20): # Delete all in field before updating
+            self.vpReference.Edit0.type_keys("{BACKSPACE}")
+
+        self.vpReference.Edit0.type_keys(claimNum)
+        if search: self.vpReference.SearchButton.click()
+        if log: print("Claim Number Updated")
+        if checkWrongClaim:
+            sleep(timeout)
+            try: # Attempt to connect to see if claim number failed to search
+                self.applicationReference.connect(title_re="Viewer Printer") 
+            except ElementAmbiguousError:
+                self.reference.ViewerPrinter.OK.click() # Click OK on error Dialouge
+                return False
+        
+        return True
+    
+    def returnX12(self, printOut:bool=False, log:bool=False):
+        '''
+        returnx x12 data
+        args:
+            printOut:bool=False -> print x12 data to console
+            log:bool=False -> print log information
+        '''
+        self.openX12()
+
+        x12 = self.vpReference.Edit3.window_text()
+        if printOut: print(x12)
+        if log: print("X12 Data Procured")
+        return x12
+
+    def openX12(self):
+        '''
+        opens the x12 data tab
+        '''
+        if self.currentTabState == VPRTypes.VPR_X12:
+            return
+
+        self.vpReference.Tab1TabControl.click()
+        self.vpReference.type_keys("{VK_LEFT}")
+        self.currentTabState = VPRTypes.VPR_X12
+
+    def openForm(self):
+        '''
+        opens the form data tab
+        '''
+        if self.currentTabState == VPRTypes.VPR_FORM:
+            return
+
+        self.vpReference.Tab1TabControl.click()
+        self.vpReference.type_keys("{VK_RIGHT}")
+        self.currentTabState = VPRTypes.VPR_FORM
+
+if __name__ == "__main__":
+    '''
+    Testing Information
+    '''
+    controller = VPRController()
+    getClaim = controller.updateClaimNum("305631381800", True, True)
+    if getClaim:
+        x12 = controller.returnX12()
+        controller.openX12()
+        controller.openForm()
+        print(x12 != True) # Check if x12 string is empty
+    else:
+        print("Claim Failed to Load")
